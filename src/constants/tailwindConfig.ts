bbe85672d9091db67b6293204d5ec21bea7bb3b4
Zograb Mkrtchyan <mkrtch.zograb@gmail.com>
2023-08-19 23:04:53 +0400

    🔧 add tailwind config variables to constant

---
+++ src/constants/tailwindConfig.ts
@@ -0,0 +1,6 @@
+import resolveConfig from 'tailwindcss/resolveConfig';
+import tailwindConfig from '../../tailwind.config';
+
+const fullConfig = resolveConfig(tailwindConfig);
+export const SCREENS = fullConfig.theme?.screens as { [key: string]: string };
+export const COLORS = fullConfig.theme?.colors as { [key: string]: string };
