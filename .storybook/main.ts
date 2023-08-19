48d6fd5a6a634a2f8363984794ca06576082e086
Zograb Mkrtchyan <mkrtch.zograb@gmail.com>
2023-08-19 22:32:24 +0400

    🔧 add aliases to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -33,5 +34,11 @@ const config: StorybookConfig = {
       "@babel/plugin-transform-react-jsx"
     ],
   }),
+  webpackFinal: async (config: any) => {
+    // Add path aliases
+    config.resolve.alias["@"] = path.resolve(__dirname, "../src");
+
+    return config;
+  },
 };
 export default config;
