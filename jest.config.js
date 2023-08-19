d638c16aa9e65a95f86c4ec3789449615713ac1c
Zograb Mkrtchyan <mkrtch.zograb@gmail.com>
2023-08-19 21:32:28 +0400

    🔧 add file transform to jest for media

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,12 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv : ['<rootDir>/jest.setup.js'],
   transform: {
-    '\\.[jt]sx?$': ['babel-jest', { configFile: './babel.config.testing.js' }]
-  }
+    '\\.[jt]sx?$': ['babel-jest', { configFile: './babel.config.testing.js' }],
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/__mocks__/fileTransformer.js'
+  },
+  moduleNameMapper: {
+    "^@/(.+)$": "<rootDir>/src/$1"
+  },
+  transformIgnorePatterns: ['node_modules'],
 }
