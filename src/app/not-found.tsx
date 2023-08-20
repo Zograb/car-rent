44a52ed84c0158680b097758727613ed32e4e5da
Zograb Mkrtchyan <mkrtch.zograb@gmail.com>
2023-08-20 14:49:11 +0400

    💄 update not found page ui

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,16 @@
+'use client';
+
 import Link from 'next/link';
+import tw from 'twin.macro';
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div css={[
+      tw`flex justify-center items-center flex-col`,
+      tw`h-screen w-screen`,
+    ]}>
+      <h2 tw="text-center pt-12 text-6xl">Not found</h2>
+      <Link href="/" tw="mt-4 text-2xl">Return Home</Link>
     </div>
   );
 }
