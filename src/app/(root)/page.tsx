492a1c88e5813a817b99ad339120f5e35bc45af9
Zograb Mkrtchyan <mkrtch.zograb@gmail.com>
2023-08-21 16:16:38 +0400

    ✨ update dashboard layout

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,24 @@
+'use client';
+
+import 'twin.macro';
+
+import {
+  CarAvailablity,
+  EarningSummary,
+  LiveCarStatus,
+  TodayStatistics,
+} from '@/containers';
+
 const RootPage = () => {
   return (
-    <div>Dashboard</div>
+    <div tw="block grid-cols-dashboard xl:grid">
+      <TodayStatistics />
+      <div tw="px-4 pb-12 overflow-hidden sm:px-7 lg:pb-0">
+        <CarAvailablity />
+        <LiveCarStatus />
+        <EarningSummary />
+      </div>
+    </div>
   );
 }
 
