cfae267fb8eb8ed55141c4cd2ca93605b20a34c8
Zograb Mkrtchyan <mkrtch.zograb@gmail.com>
2023-08-21 16:09:11 +0400

    🏷️ update twin global types

--- src/types/twin.d.ts
+++ src/types/twin.d.ts
@@ -1,17 +1,32 @@
-import styledImport, { CSSProp, css as cssImport } from 'styled-components'
-
+import type { DOMAttributes } from 'react';
+import styledImport, { CSSProp, css as cssImport } from 'styled-components';
 import 'twin.macro';
 
 declare module 'twin.macro' {
   // The styled and css imports
-  const styled: typeof styledImport;
-  const css: typeof cssImport;
+  const styled: typeof styledImport
+  const css: typeof cssImport
 }
 
 declare module 'react' {
-  // The tw and css prop
-  interface Attributes {
-    tw?: string;
+  // The css prop
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
     css?: CSSProp;
+    tw?: string;
+  }
+  // The inline svg css prop
+  interface SVGProps extends SVGProps<SVGSVGElement> {
+    css?: CSSProp;
+    tw?: string;
+  }
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicAttributes {
+      as?: string;
+      css?: CSSProp;
+      tw?: string;
+    }
   }
 }
